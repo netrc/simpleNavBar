6ac9f17dab0913f4ac45400b80164dc44c63cd61
netrc <ric@netrc.com>
2017-10-07 12:44:28 -0400

    with test harness; use SNRDEBUG env var for full debug output

--- test.js
+++ test.js
@@ -3,57 +3,74 @@
 
 const snr = require('./simpleNavRouter')
 
+global.SNRDEBUG = process.env.hasOwnProperty('SNRDEBUG') ? true : false;
+
+function snrDebug( s ) {
+	global.SNRDEBUG && console.log(s);
+}
+//console.log(`snrdebug: ${global.SNRDEBUG}`);
+
 //console.log(`snr version: ${snr._version}`);
 
 // mock $ func, w,  append func, empty func  // force into root/global context
 global.$  = function  (n) {
-	console.log(`making jq proxy for ${n}`);
+	snrDebug(`making jq proxy for ${n}`);
 	return {
 		myname: n,
 		empty: function() {
-			console.log(`$ proxy ${this.myname}: empty`);
+			snrDebug(`$ proxy ${this.myname}: empty`);
 		},
 		append: function( el ) {
-			console.log(`$ proxy ${this.myname} append: ${el}`);
+			snrDebug(`$ proxy ${this.myname} append: ${el}`);
 			return global.$(el);  // careful not to recurse!
 		}
 	}
 }
+
+// fake ga func, fake global location.href
 global.window = {};   // mock to store onhashchange
 global.location = { href: '' };   // mock to store location
 global.ga = function ( s ) {
-	console.log(`ga mock: ${s}`);
+	snrDebug(`ga mock: ${s}`);
 }
 
-// fake ga func
-// fake global location.href
-
+///// little test harness
 function testRoute(r) {
-	console.log(`snr testRoute: ${r}`);
+	global.snrResult = '';
+	snrDebug(`snr testRoute: ${r}`);
         location.href=r;
         //snr.doRouting()
 		window.onhashchange();
-	console.log(``);
+	snrDebug(``);
 }
+function testCheck(s) {
+	snrDebug(`tc: s: ${s}  snrResult: ${global.snrResult}`);
+	(global.snrResult == s) || console.error(`failed: ${s}`);
+}
+///// little test harness
+
 
 // test setup
-
 //// test data
 
 function doIntro ( divEl ) {  // just draw what you want
-	console.log(`doing Intro on divEl`);
+	snrDebug(`doing Intro on divEl`);
+	global.snrResult = 'doIntro ok';
 }
 
 function doDiary ( divEl ) {  // just draw what you want
-	console.log(`doing Diary on divEl`);
+	snrDebug(`doing Diary on divEl`);
+	global.snrResult = 'doDiary ok';
 }
 
 function doPeople ( divEl ) {  // just draw what you want
-	console.log(`doing People on divEl`);
+	snrDebug(`doing People on divEl`);
+	global.snrResult = 'doPeople ok';
 }
 
 function doPrint ( divEl ) {  // just draw what you want
-	console.log(`doing Print on divEl`);
+	snrDebug(`doing Print on divEl`);
+	global.snrResult = 'doPrint ok';
 }
 
 var appRoutingData = {
@@ -68,6 +85,10 @@ var appRoutingData = {
 snr("testAppDiv", "testSNR App", appRoutingData);
 
 testRoute(`index.html`);
+	testCheck('doDiary ok');
 testRoute(`index.html#Introduction`);
+	testCheck('doIntro ok');
 testRoute(`index.html#People`);
+	testCheck('doPeople ok');
 testRoute(`index.html#Print`);
+	testCheck('doPrint ok');
